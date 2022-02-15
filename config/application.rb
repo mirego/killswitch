117c8f1fc8af9b028ddb5eef2d523aa485d28cfc
Rémi Prévost <rprevost@mirego.com>
2022-02-15 09:26:10 -0500

    v1.0.4

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ Bundler.require(:default, Rails.env)
 module Killswitch
   class Application < Rails::Application
     # Version
-    VERSION = '1.0.3'.freeze
+    VERSION = '1.0.4'.freeze
 
     config.load_defaults 6.0
 
