99c55e3c3e9863d885d0c4219c907bd6effc01a6
Rémi Prévost <remi@exomel.com>
2022-09-14 10:50:54 -0400

    v1.0.5

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ Bundler.require(:default, Rails.env)
 module Killswitch
   class Application < Rails::Application
     # Version
-    VERSION = '1.0.4'.freeze
+    VERSION = '1.0.5'.freeze
 
     config.load_defaults 6.0
 
