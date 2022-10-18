e191438bd064b2a235a1ee1d62ef0e9096ed9689
Rémi Prévost <rprevost@mirego.com>
2022-10-18 19:55:25 -0400

    Add orphan user message for user without organizations (#24)

--- app/controllers/web/home_controller.rb
+++ app/controllers/web/home_controller.rb
@@ -5,6 +5,8 @@ class Web::HomeController < Web::ApplicationController
     return if current_user.blank?
 
     organization = current_user.organizations.first
+    return if organization.blank?
+
     redirect_to web_organization_applications_path(organization_id: organization)
   end
 end
